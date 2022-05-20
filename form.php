bc86ca565ebe89dda71fcf7c460b571a0d772f52
ivan diaz <ivandiazdeveloper@hotmail.com>
2022-05-19 23:38:37 -0300

    form functionality

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$commentary = $_POST['commentary'];
+
+$commentary = "Este mensaje fue enviado por " . $firstname . ",\r\n";
+$commentary .= "Su e-mail es: " . $email . " \r\n";
+$commentary .= "Mensaje: " . $_POST['commentary'] . " \r\n";
+
+$destination = 'ivandiazdeveloper@gmail.com';
+$from = 'Este email fue enviado desde la web Zierk Portfolio';
+
+mail($destination, $from, utf8_decode($commentary), $header);
+
+header('Location:success.html');
+
+?>
