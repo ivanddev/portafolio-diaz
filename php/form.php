ce8fa64eaf76f641db076dd03837f9e5e0be1aa6
ivan diaz <ivandiazdeveloper@hotmail.com>
2022-05-26 22:47:49 -0300

    fixform2

--- php/form.php
+++ php/form.php
@@ -5,6 +5,8 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $commentary = $_POST['commentary'];
 
+$header .= "Content-Type: text/plain";
+
 $commentary = "Este mensaje fue enviado por " . $firstname . ",\r\n";
 $commentary .= "Su e-mail es: " . $email . " \r\n";
 $commentary .= "Mensaje: " . $_POST['commentary'] . " \r\n";
@@ -12,7 +14,7 @@ $commentary .= "Mensaje: " . $_POST['commentary'] . " \r\n";
 $destination = 'ivandiazdeveloper@gmail.com';
 $from = 'Este email fue enviado desde la web Zierk Portfolio';
 
-mail($destination, $from, utf8_decode($commentary), header);
+mail($destination, $from, utf8_decode($commentary), $header);
 
 header('Location:success.html');
 
